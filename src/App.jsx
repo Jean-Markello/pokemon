c5c18e20cc33bd318224f0746aaa785b2d57473a
Markello <jleta084@uottawa.ca>
2024-03-19 23:53:53 -0400

    implemeting api call to server, setting http request

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,47 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Details from './Details';
-import { ChakraProvider } from '@chakra-ui/react'
-import theme from './theme'
+import Pokemon from './Pokemons';
+import { Box, Heading } from '@chakra-ui/react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: "http://localhost:4000/graphql" }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <div>
-        <Details />
-      </div>
-    </ChakraProvider>
-
+    <ApolloProvider client={client}>
+      <Router>
+        <Box maxW={1000} maxH="100%" mx="auto" px={10} pt={10} fontSize="sm" style={{ minHeight: '100vh' }}>
+          <Heading mb={10}>Pokemon List</Heading>
+          <Routes>
+            <Route exact path="/" element={<Details />} />
+            <Route path="/pokemon/:pokemonName" element={<Pokemon />} />
+          </Routes>
+        </Box>
+      </Router>
+    </ApolloProvider>
   );
 }
 
